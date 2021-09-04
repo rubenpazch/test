7ef82931233c6ab4ea45c3ae2b3aba6a63a59859
rubenpaz <ruben.paz@vascarsolutions.com>
2021-09-04 14:51:27 -0500

    add subtitle in java script example

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello Worlds!';
+myHeading.textContent = 'Hello World!';
+
+const mySubHeading= document.querySelector('h2');
+mySubHeading.textContent = 'bye World!';
