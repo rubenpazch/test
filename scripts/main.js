10e90661d7b6a25b1bf89ebac5ea3e6a5351e42a
ruben paz chuspe <rubenpazchuspe@outlook.com>
2021-09-04 16:13:37 -0500

    adding new function

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,25 @@ myImage.onclick = function() {
       myImage.setAttribute('src','images/firefox-icon.png');
     }
 }
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("please enter your name");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "mozilla is cool,"+myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} 
+else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
